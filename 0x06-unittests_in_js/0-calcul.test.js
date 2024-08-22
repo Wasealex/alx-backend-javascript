1305dfc9bda8c849ce3d251acc048fa3a465f847
Wasealex <wasealex27@gmail.com>
2024-08-22 07:53:24 +0300

    fixed eslint linter and created a new test for calcul using different types

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -6,18 +6,18 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(6.0, 5.0), 11);
   });
 
-    it('rounds down numbers', () => {
-	assert.strictEqual(calculateNumber(3.4, 4.4), 7);
-    });
+  it('rounds down numbers', () => {
+    assert.strictEqual(calculateNumber(3.4, 4.4), 7);
+  });
 
-    it('rounds up numbers', () => {
-	assert.strictEqual(calculateNumber(3.6, 4.6), 9);
-    });
+  it('rounds up numbers', () => {
+    assert.strictEqual(calculateNumber(3.6, 4.6), 9);
+  });
 
-    it('rounds down the first number and rounds up the second', () => {
-	assert.strictEqual(calculateNumber(3.4, 4.6), 8);
-    });
-    it('trialling numbers of float numbers rounding up/down', () => {
-	assert.strictEqual(calculateNumber(3.4666666, 4.6666666), 8)
-    })
+  it('rounds down the first number and rounds up the second', () => {
+    assert.strictEqual(calculateNumber(3.4, 4.6), 8);
+  });
+  it('trialling numbers of float numbers rounding up/down', () => {
+    assert.strictEqual(calculateNumber(3.4666666, 4.6666666), 8);
+  });
 });
---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  describe('type === "SUM"', () => {
+    it('adding positive numbers', () => {
+      assert.strictEqual(calculateNumber('SUM', 2.0, 4.0), 6);
+    });
+  });
+});
+
+describe('calculateNumber', () => {
+  describe('type === "SUBTRACT"', () => {
+    it('subtracting positive numbers', () => {
+	    assert.strictEqual(calculateNumber('SUBTRACT', 2.0, 1.0), 1);
+    });
+  });
+});
+
+describe('calculateNumber', () => {
+  describe('type === "DIVIDE"', () => {
+    it('divide postive non zero numbers', () => {
+	    assert.strictEqual(calculateNumber('DIVIDE', 6.0, 3.0), 2);
+    });
+  });
+});
