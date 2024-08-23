1b06e29236e79cf5014885ada9aaade72d96a581
Wasealex <wasealex27@gmail.com>
2024-08-23 14:28:45 +0300

    fixed the sentence on the test

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -4,10 +4,10 @@ const { expect } = require('chai');
 describe('aPI integration test', () => {
   const API_URL = 'http://localhost:7865';
 
-  it('gET / and return a response', () => new Promise((done) => {
+  it('gET / and return a response', ((done) => {
     request.get(`${API_URL}/`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
-      expect(body).to.be.equal('Welcome to get payment system');
+      expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
   }));
