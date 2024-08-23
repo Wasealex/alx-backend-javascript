4a25d0a0bb9f0ee7d5fa5d29a50d692ec5d234a2
Wasealex <wasealex27@gmail.com>
2024-08-23 16:49:16 +0300

    used json()

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 7865;
 
+app.use(express.json());
 app.get('/', (_, res) => {
   res.send('Welcome to the payment system');
 });
