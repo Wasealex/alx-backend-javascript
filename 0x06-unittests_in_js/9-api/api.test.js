434cffac7954cb98f8fb9e091c449295ba41cfda
Wasealex <wasealex27@gmail.com>
2024-08-23 16:27:50 +0300

    fixed test of 9-api error in the test because of mismathced ()

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -4,26 +4,25 @@ const { expect } = require('chai');
 describe('aPI integration test', () => {
   const API_URL = 'http://localhost:7865';
 
-  it('gET / and return a response', ((done) => {
+  it('gET / and return a response', (() => new Promise((done) => {
     request.get(`${API_URL}/`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
+  })));
+    it('gET /cart/:id returns correct response for valid :id', () => new Promise((done) => {
+      request.get(`${API_URL}/cart/27`, (_err, res, body) => {
+        expect(res.statusCode).to.be.equal(200);
+        expect(body).to.be.equal('Payment methods for cart 27');
+        done();
+      });
+    }));
 
-  it('GET /cart/:id returns correct response for valid :id', (done) => {
-    request.get(`${API_URL}/cart/27`, (_err, res, body) => {
-      expect(res.statusCode).to.be.equal(200);
-      expect(body).to.be.equal('Payment methods for cart 27');
-      done();
-    });
-  });
-
-  it('GET /cart/:id returns 404 response for NOT a number in :id', (done) => {
-    request.get(`${API_URL}/cart/NOT`, (_err, res, _body) => {
-      expect(res.statusCode).to.be.equal(404);
-      done();
-    });
-  });
-  }));
+    it('gET /cart/:id returns 404 response for NOT a number in :id', () => new Promise((done) => {
+      request.get(`${API_URL}/cart/NOT`, (_err, res, _body) => {
+        expect(res.statusCode).to.be.equal(404);
+        done();
+      });
+    }));
 });
